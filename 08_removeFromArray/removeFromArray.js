276c95cd4f1aa45931fbf8aec82ab7f4b9c0b6d2
jillaye <jillaye@gmail.com>
2025-12-07 18:23:24 -0700

    08_removeFromArray solution

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,6 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    const newArray = array.filter(item => !args.includes(item));
+    return newArray;
 };
 
 // Do not edit below this line
