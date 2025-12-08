c01733cce9292da1409404929926d19d7f9176fe
jillaye <jillaye@gmail.com>
2025-12-07 19:15:54 -0700

    exercies 09_sumAll solution

--- 09_sumAll/sumAll.js
+++ 09_sumAll/sumAll.js
@@ -1,5 +1,23 @@
-const sumAll = function() {
+function isPositiveInteger(value) {
+    return Number.isInteger(value) && value > 0;
+}
 
+const sumAll = function(a, b) {
+    if (isPositiveInteger(a) && isPositiveInteger(b)) {
+        if (a === b) {
+            return a;
+        }
+        let sum = 0;
+        if (b > a) {
+            for (let x = a; x <= b; x++) {
+                sum += x; 
+            }
+            return sum;
+        } else return sumAll (b, a);
+    }
+    else {
+        return ("ERROR")
+    }
 };
 
 // Do not edit below this line
