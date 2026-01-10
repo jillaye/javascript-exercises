be45a2f891bbde736ff53e64b1c136802acde89c
jillaye <jillaye@gmail.com>
2026-01-09 21:10:31 -0700

    my exercise 13 solution

--- 13_palindromes/palindromes.js
+++ 13_palindromes/palindromes.js
@@ -1,5 +1,16 @@
-const palindromes = function () {
-
+const palindromes = function (input) {
+    input = input.toLowerCase();
+    let chars = input.match(/[a-zA-Z0-9]/g)
+    isPalindrome = true;
+    end = chars.length - 1;
+    mid = Math.ceil(end/2);
+    for (let i = 0; i < mid; i++) {
+        if (chars[i] !== chars[end -i]) {
+            isPalindrome = false;
+            break
+        }
+    }
+    return isPalindrome;
 };
 
 // Do not edit below this line
